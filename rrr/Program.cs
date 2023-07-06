145662079063c7eb8f5570da07eb646d5dcb5912
beerkozturk <122112098+beerkozturk@users.noreply.github.com>
2023-07-06 12:14:40 +0300

    program cs edited

--- rrr/Program.cs
+++ rrr/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace rrr;
+﻿using System;
+
+namespace rrr;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var number = 2;
+        int count = 10;
+        float totalPrice = 20.95f;
+        char character = 'A';       //tek tırnak karakter
+        string firstName = "Mosh";  //çift tırnak string
+        bool isWorking = false;
+
+        Console.WriteLine(number);
+        Console.WriteLine(count);
+        Console.WriteLine(totalPrice);
+        Console.WriteLine(character);
+
+
+        s
+        Console.WriteLine(firstName);
+        Console.WriteLine(isWorking);
+
     }
 }
 
