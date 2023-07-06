36483f9bb61d2a0dbd38b8bd43287cc30ffddcd8
beerkozturk <122112098+beerkozturk@users.noreply.github.com>
2023-07-06 16:02:57 +0300

    Add files via upload
    Type Conversion

---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TypeConversion;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        byte b = 1;
+        int i = b;
+        Console.WriteLine(i);
+
+        int a = 1;
+        byte c = (byte) a;
+        Console.WriteLine(c);
+    }
+}
+
